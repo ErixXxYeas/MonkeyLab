0cda2935391280e0193b6cdcb90522aa19c1b45b
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-11-19 11:38:47 +0100

    feature - general API functionality

---
+++ src/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongo = require("mongodb").MongoClient;
+const url = "mongodb://localhost";
+
+let db;
+
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error("Bling: " + err);
+      return;
+    } else {
+      console.log("found");
+    }
+    db = client.db("testdb");
+    console.log("running");
+  }
+);
+
+const port = 3000;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const students = [
+  { id: 1, name: "Eren Yaeger" },
+  { id: 2, name: "Levi Ackermann" },
+];
+
+app.get("/surveys", (req, res) => {
+  res.send(students);
+});
+
+app.get("/surveys/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(students);
+});
+
+app.post("/surveys", (req, res) => {
+  const content = req.body;
+  students.push(content);
+  res.send(content);
+});
+
+app.listen(port, () => console.log("Server ready"));
