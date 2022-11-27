ecde55ee94e56f00e2ac7b2ff7b55c6af2a542a9
ErixXxYe <derstylervonmorgen@gmail.com>
2022-11-27 08:31:11 +0100

    devlope - fixed admin page disapearing during merge

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
+import AdminPage from "./AdminPage.js";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
