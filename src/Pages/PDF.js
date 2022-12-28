93f821efd8399debd4dc1432fed7ddc018ea566e
Eric <eric.ye@student.htldornbirn.at>
2022-12-28 17:27:03 +0100

    feature - Eval_to_PDF image can be printed

--- src/Pages/PDF.js
+++ src/Pages/PDF.js
@@ -5,27 +5,36 @@ import Butt from "../components/button";
 import Printer from "../components/printer";
 import { renderToString } from "react-dom/server";
 import N29 from "./N-29_Test";
+import domtoimage from "dom-to-image";
 
 const PDF = () => {
   const doc = new jsPDF({ orientation: "landscape" });
 
   const test = () => {
-    const string = renderToString(<N29></N29>);
-    doc.html(string, {
-      callback: function (doc) {
-        doc.save("test-Print");
-      },
+    var node = document.getElementById("print");
+    domtoimage.toPng(node).then(function (dataUrl) {
+      var img = new Image();
+      img.src = dataUrl;
+      doc.addImage(img, "Png", 0, 0, 0, 0);
+      doc.autoPrint(doc);
+      doc.save("ben");
     });
     console.log("tested");
   };
+
+  const test2 = () => {
+    window.print;
+  };
+
   return (
     <React.Fragment>
       <div id="print">
         <p className={css.h1}>Hello World</p>
-      </div>
-      <div>
-        <Butt name={"print"} event={test}></Butt>
-        <button onClick={test}>Test</button>
+
+        <div>
+          <Butt name={"JSPDF"} event={test}></Butt>
+          <button onClick={test2}>Regular</button>
+        </div>
       </div>
     </React.Fragment>
   );
